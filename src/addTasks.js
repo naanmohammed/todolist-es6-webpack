43b66e24a29943ba3b9ce53d3516851534c569e6
Naan Mohammed <naanahmed24@gmail.com>
2023-04-06 20:35:05 +0300

    Add create task function

---
+++ src/addTasks.js
@@ -0,0 +1,16 @@
+export function addTask(tasks) {
+    const taskInput = document.getElementById('task-input');
+    const taskDescription = taskInput.value.trim();
+  
+    if (taskDescription.length > 0) {
+      const newTask = {
+        description: taskDescription,
+        completed: false,
+        index: tasks.length,
+      };
+  
+      tasks.push(newTask);
+      taskInput.value = '';
+    }
+  }
+  
